907c311f14f6155bd97ab6c96922a7184138bd73
wooWoow <wuhaoyang9307@163.com>
2020-06-02 22:11:28 +0800

    接口添加统一前缀

--- src/app.js
+++ src/app.js
@@ -1,85 +1,87 @@
-import Koa from 'koa'
-import session from 'koa-session'
-import views from 'koa-views'
-import json from 'koa-json'
-import onerror from 'koa-onerror'
-import bodyparser from 'koa-bodyparser'
-import koaLogger from 'koa-logger'
-import path from 'path'
-import jwt from 'koa-jwt'
-import fs from 'fs'
-import {errorRoute, notFoundRoute} from './middleware/errorRouteCatch'
-import {accessLogger, logger} from '../config/logger.config'
-import sessConfig from '../config/session.config'
-import routers from './routes/index'
+import Koa from "koa";
+import session from "koa-session";
+import views from "koa-views";
+import json from "koa-json";
+import onerror from "koa-onerror";
+import bodyparser from "koa-bodyparser";
+import koaLogger from "koa-logger";
+import path from "path";
+import jwt from "koa-jwt";
+import fs from "fs";
+import { errorRoute, notFoundRoute } from "./middleware/errorRouteCatch";
+import { accessLogger, logger } from "../config/logger.config";
+import sessConfig from "../config/session.config";
+import routers from "./routes/index";
 
 // error handler
-const app = new Koa()
-const publicKey = fs.readFileSync(path.join(__dirname, '../publicKey.pub'))
-onerror(app)
-logger.error('start app', 'env:', process.env.NODE_ENV) 
+const app = new Koa();
+const publicKey = fs.readFileSync(path.join(__dirname, "../publicKey.pub"));
+onerror(app);
+logger.error("start app", "env:", process.env.NODE_ENV);
 // middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(accessLogger())
-app.use(koaLogger())
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
+app.use(json());
+app.use(accessLogger());
+app.use(koaLogger());
 
 // router error
-app.use(errorRoute())
+app.use(errorRoute());
 
-app.use(require('koa-static')(__dirname + '../assets'))
+app.use(require("koa-static")(__dirname + "../assets"));
 
 // set the signature, control api to be accessed
-app.use(jwt({ 
-  secret: publicKey.toString()
-}).unless({
-  path: [
-    /^\/users\/login/,
-    /^\/home/,
-    /^\/assets/
-  ] 
-}))
+app.use(
+  jwt({
+    secret: publicKey.toString(),
+  }).unless({
+    path: [/^\/v1\/users\/login/, /^\/v1\/home/, /^\/v1\/assets/],
+  })
+);
 
 // set template type, if use hbs, only change "extension: 'hbs'" and "hbs: 'handlebars'"
-app.use(views(__dirname + '/views', {
-  extension: 'html',
-  map: { html: 'handlebars' }
-}))
+app.use(
+  views(__dirname + "/views", {
+    extension: "html",
+    map: { html: "handlebars" },
+  })
+);
 
 // extends request header info
 app.use(async (ctx, next) => {
-  ctx.request.header.publicKey = publicKey.toString()
-  // test authorization 
+  ctx.request.header.publicKey = publicKey.toString();
+  // test authorization
   // ctx.request.header = {'authorization': "Bearer " + (ctx.request.headers.)}
-  await next()
-})
+  await next();
+});
 
 // if donot use session, you can remove the session middleware here.
-app.keys = [publicKey.toString()]
-app.use(session(sessConfig, app))
+app.keys = [publicKey.toString()];
+app.use(session(sessConfig, app));
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 
 // routes
-routers.forEach(router => {
-  app.use(router.routes(), router.allowedMethods())
-})
+routers.forEach((router) => {
+  app.use(router.routes(), router.allowedMethods());
+});
 
 // not found router
-app.use(notFoundRoute())
+app.use(notFoundRoute());
 
 // error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-  logger.error('server error', err, ctx)
-})
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+  logger.error("server error", err, ctx);
+});
 
-module.exports = app
+module.exports = app;
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,22 +1,22 @@
-import koaRouter from 'koa-router'
-const router = koaRouter()
+import koaRouter from "koa-router";
+const router = koaRouter();
 
-router.prefix('/home')
+router.prefix("/v1/home");
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+router.get("/", async (ctx, next) => {
+  await ctx.render("index", {
+    title: "Hello Koa 2!",
+  });
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/string", async (ctx, next) => {
+  ctx.body = "koa2 string";
+});
 
-router.get('/json', async (ctx, next) => {
+router.get("/json", async (ctx, next) => {
   ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    title: "koa2 json",
+  };
+});
 
-export default router
+export default router;
